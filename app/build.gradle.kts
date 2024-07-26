2090fabf44b34b6c534e49af140f9caefe8c009a
Tiê Proost <tie.proost@student.hogent.be>
2024-07-26 10:10:08 +0200

    update AGP to 8.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.tieproost.fitnessapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tieproost.fitnessapp"
