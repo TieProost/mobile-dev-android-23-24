01dd478c5f465dbc17abc7f695d7de263c87a322
Tiê Proost <tie.proost@student.hogent.be>
2024-07-27 14:10:15 +0200

    add navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    // navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
     // extra icons
     implementation("androidx.compose.material:material-icons-extended")
 }
