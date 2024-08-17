c4d163ea2936d3d94b97389b50b0e69b4e3b1085
Tiê Proost <tie.proost@student.hogent.be>
2024-08-17 20:37:08 +0200

    modify LoadingScreen

--- app/src/main/java/com/tieproost/fitnessapp/ui/common/LoadingScreen.kt
+++ app/src/main/java/com/tieproost/fitnessapp/ui/common/LoadingScreen.kt
@@ -1,9 +1,28 @@
 package com.tieproost.fitnessapp.ui.common
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoadingScreen()  {
-    Text("Loading placeholder...")
+fun LoadingScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+            Modifier.fillMaxSize().padding(36.dp).background(MaterialTheme.colorScheme.background.copy(alpha = 0.7f)),
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.fillMaxWidth(0.25f),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
 }
